be68428fb1a4c32176233c80ddcb44ce320a3dd4
David Czarnecki <dczarnecki@agoragames.com>
2011-06-28 10:55:55 -0400

    This should remove any deprecation warnings in embedded applications.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
-Rails::Application.routes.draw do
-  # Unrecoverable error
-  match '/error' => "application#errship_standard", :as => :error
+if defined?(Rails::Application)
+  Rails.application.routes.draw do
+    # Unrecoverable error
+    match '/error' => "application#errship_standard", :as => :error
 
-  # Rails currently (until 3.1) ignores rescue_from ActionController::RoutingError, so render 404 manually
-  match '*address' => 'application#render_404_error' unless Rails.application.config.consider_all_requests_local
+    # Rails currently (until 3.1) ignores rescue_from ActionController::RoutingError, so render 404 manually
+    match '*address' => 'application#render_404_error' unless Rails.application.config.consider_all_requests_local
+  end
 end
