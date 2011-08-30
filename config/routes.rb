1ad5e8411eb1f3bdbef9f30fc3e931d0cebd6c20
Logan Koester <lkoester@agoragames.com>
2011-08-30 18:08:18 -0400

    Changes /404 to /not_found

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ if defined?(Rails::Application)
   Rails.application.routes.draw do
     # Unrecoverable error
     match '/error' => "application#errship_standard", :as => :error
-    match '/404' => "application#render_404_error"
+    match '/not_found' => "application#render_404_error", :as => 'not_found'
 
     # Rails currently (until 3.1) ignores rescue_from ActionController::RoutingError, so render 404 manually
     match '*address' => 'application#render_404_error' unless Rails.application.config.consider_all_requests_local
