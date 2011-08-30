9f4697af34133d549c127033745de21bc84599d4
Logan Koester <lkoester@agoragames.com>
2011-08-30 18:06:54 -0400

    Adds /404 routegs

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ if defined?(Rails::Application)
   Rails.application.routes.draw do
     # Unrecoverable error
     match '/error' => "application#errship_standard", :as => :error
+    match '/404' => "application#render_404_error"
 
     # Rails currently (until 3.1) ignores rescue_from ActionController::RoutingError, so render 404 manually
     match '*address' => 'application#render_404_error' unless Rails.application.config.consider_all_requests_local
